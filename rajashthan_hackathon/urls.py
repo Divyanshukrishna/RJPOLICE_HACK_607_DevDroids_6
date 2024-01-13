d5139a65ec18e02f8a23e23a5431b301936a8b9a
divyanshu <divyanshukrishna11@gmail.com>
2024-01-13 12:10:19 +0530

    imagesds

--- rajashthan_hackathon/urls.py
+++ rajashthan_hackathon/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path('',include('myapp.urls')),
     path('accounts/', include('allauth.urls')),
 
-] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
